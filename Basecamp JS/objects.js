5796d31d5e3b7b96664c266c3dcaad338ed0ce31
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-06-19 17:56:09 -0300

    Função que substitui números pares por 0

--- Basecamp JS/objects.js
+++ Basecamp JS/objects.js
@@ -11,25 +11,43 @@
 
 //Verificando palíndromo
 
-function verificaPalindromo(string) {
-    if(!string) return "string inexistente"
+// function verificaPalindromo(string) {
+//     if(!string) return "string inexistente"
 
-    stringReversa = string.split("").reverse().join("")
-    stringReversa === string ? console.log('É um palíndromo') : console.log('Não é um palíndromo')
-}
+//     stringReversa = string.split("").reverse().join("")
+//     stringReversa === string ? console.log('É um palíndromo') : console.log('Não é um palíndromo')
+// }
 
-verificaPalindromo('natan')
+// verificaPalindromo('natan')
 
-function verificaPalindromo2(string) {
-    if(!string) return "string inexistente"
+// function verificaPalindromo2(string) {
+//     if(!string) return "string inexistente"
 
-    for (let i = 0; i < string.length / 2; i++) {
-        if (string[i] !== string[string.length - 1 - i]) {
-            return 'Não é um palíndromo'
+//     for (let i = 0; i < string.length / 2; i++) {
+//         if (string[i] !== string[string.length - 1 - i]) {
+//             return 'Não é um palíndromo'
+//         }
+//     }
+
+//     return 'É um palíndromo'
+// }
+
+// console.log(verificaPalindromo2('ovo'))
+
+function substituiPares(array) {
+    if (!array) return -1;
+    if (!array.length) return -1;
+    
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 0) {
+            console.log('Você já é zero')
+        } else if (array[i] % 2 === 0) {
+            console.log(`Substituindo ${array[i]} por 0`)
+            array[i] = 0;
         }
     }
-
-    return 'É um palíndromo'
+    return array
 }
 
-console.log(verificaPalindromo2('ovo'))
+let array = [1, 2, 3, 4, 5, 6]
+console.log(substituiPares(array))
