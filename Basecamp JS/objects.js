9d59023be89149ccee23339690890c75306ef447
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-06-19 17:09:52 -0300

    Criando verificador de palíndromo

--- Basecamp JS/objects.js
+++ Basecamp JS/objects.js
@@ -1,9 +1,35 @@
-var bancoDeDados = {nome: 'Robson', idade: 46, nacionalidade: 'Brasil', sexo: 'Masculino'} //Um object pode armazenar inclusive arrays e outros objects
-console.log(bancoDeDados)
-console.log(bancoDeDados.idade)
+// var bancoDeDados = {nome: 'Robson', idade: 46, nacionalidade: 'Brasil', sexo: 'Masculino'} //Um object pode armazenar inclusive arrays e outros objects
+// console.log(bancoDeDados)
+// console.log(bancoDeDados.idade)
 
-console.log(`${bancoDeDados.nome} tem ${bancoDeDados.idade} anos, nasceu no ${bancoDeDados.nacionalidade} e é do sexo ${bancoDeDados.sexo}`)
+// console.log(`${bancoDeDados.nome} tem ${bancoDeDados.idade} anos, nasceu no ${bancoDeDados.nacionalidade} e é do sexo ${bancoDeDados.sexo}`)
 
-var {idade, nacionalidade} = bancoDeDados //Seleciona apenas os atributos desejados para serem exibidos
-console.log(idade, nacionalidade)
-console.log(bancoDeDados)
+// var {idade, nacionalidade} = bancoDeDados //Seleciona apenas os atributos desejados para serem exibidos
+// console.log(idade, nacionalidade)
+// console.log(bancoDeDados)
+
+
+//Verificando palíndromo
+
+function verificaPalindromo(string) {
+    if(!string) return "string inexistente"
+
+    stringReversa = string.split("").reverse().join("")
+    stringReversa === string ? console.log('É um palíndromo') : console.log('Não é um palíndromo')
+}
+
+verificaPalindromo('natan')
+
+function verificaPalindromo2(string) {
+    if(!string) return "string inexistente"
+
+    for (let i = 0; i < string.length / 2; i++) {
+        if (string[i] !== string[string.length - 1 - i]) {
+            return 'Não é um palíndromo'
+        }
+    }
+
+    return 'É um palíndromo'
+}
+
+console.log(verificaPalindromo2('ovo'))
