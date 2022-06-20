15cea0e9964bf5734aa230e75a6d8ea62de3c78e
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-06-20 08:03:32 -0300

    Trabalhando com impotação e exportação de módulos

---
+++ Basecamp JS/utilizandoModulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Gabriel", 16))
+console.log(mostraCidade("Gabriel", 'Nova Lima'))
+console.log(mostraHobby("Gabriel", 'jogar'))
