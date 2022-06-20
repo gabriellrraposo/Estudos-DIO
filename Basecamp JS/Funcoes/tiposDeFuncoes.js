565632f588fbfe07f8b6cf4890d907015e1ad379
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-06-20 08:47:25 -0300

    .

--- Basecamp JS/Funcoes/tiposDeFuncoes.js
+++ Basecamp JS/Funcoes/tiposDeFuncoes.js
@@ -1,18 +1,22 @@
 // Função comum
-function funcao() {
+function funcaoComum() {
     return "Eu sou uma função comum"
 }
+
+funcaoComum()
 
 //Função Anônima
 const soma = function (num1, num2) {
     return `A soma entre ${num1} e ${num2} é ${num1 + num2}`
 }
 
+soma(1, 3)
+
 // Função Autoinvocável --precisa estar entre parenteses e ter parenteses no final para já invocá-la
 
 (
-    function() {
-        let name = "Digital Innovation One"
-        return name
+    function(num1, num2) {
+        let res = num1 * num2
+        return res
     }
-)()
+) (2, 5);
