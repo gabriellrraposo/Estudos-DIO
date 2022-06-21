a98821f88504b7485a412d969c4eb520fbb3feef
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-06-21 08:34:55 -0300

    Utilizando this juntamente com o call

---
+++ Basecamp JS/this.js
@@ -0,0 +1,59 @@
+const pessoa = {
+    primeiroNome: 'Gabriel',
+    ultimoNome: 'Raposo',
+    id: 32,
+    nomeCompleto: function() {
+        return `${this.primeiroNome} ${this.ultimoNome}`
+    },
+    getId: function() {
+        return this.id
+    }
+}
+
+console.log(pessoa.nomeCompleto())
+console.log(pessoa.getId())
+
+////////
+
+//Utilizando Call
+
+const amigo = {
+    nome: 'Ronildo'
+}
+
+const gato = {
+    nome: 'Babigi'
+}
+
+function getName() {
+    console.log(this.nome)
+}
+
+getName.call(gato)
+
+
+const numeros = {
+    num1: 3,
+    num2: 4
+}
+
+function soma(a, b) {
+    console.log(this.num1 + this.num2 + a + b)
+}
+
+soma.call(numeros, 1, 5)
+
+//Utilizando Apply
+
+function getName2() {
+    console.log(this.nome)
+}
+
+getName.apply(amigo)
+
+
+function soma(a, b) {
+    console.log(this.num1 + this.num2 + a + b)
+}
+
+soma.call(numeros, 1, 5)
